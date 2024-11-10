e3536a1036b754bea88f4f0881ee8eb2948eaf82
KhoaVu2002 <152266289+KhoaVu2002@users.noreply.github.com>
2024-11-11 02:00:21 +0700

    Add files via upload

--- Test/Age calculator/script.js
+++ Test/Age calculator/script.js
@@ -1,17 +1,23 @@
-document.getElementById('form').addEventListener('submit', function(e){
+document.getElementById('form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const dob = new Date(document.getElementById('dob').value);
     const today = new Date();
-    
+
     let age_year = today.getFullYear() - dob.getFullYear();
     let age_month = today.getMonth() - dob.getMonth();
+    let age_day = today.getDate() - dob.getDate();
 
-    if(age_month < 0){
-        age_year--;
-        age_month += 12;
+    if (age_day < 0) {
+        const previousMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        age_day += previousMonth;
+        age_month--;
     }
 
-    document.getElementById('message').textContent = `You are ${age_year} years and ${age_month} months`
+    if (age_month < 0) {
+        age_month += 12;
+        age_year--;
+    }
 
-})
+    document.getElementById('message').textContent = `You are ${age_year} years, ${age_month} months and ${age_day} days.`;
+});
